94b68de8725e7a55e3a3ef086ece5aa153fa4296
PPTA22 <timothytwp@gmail.com>
2020-08-21 00:48:51 -0400

    fix gd ck caused big ck bug

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-
+import sys
 from time import sleep
 import time
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def clickGoldenCookie():
+def click_golden_cookie():
     try:
         driver.find_element_by_class_name('shimmer').click()
         print('[o] clicked Golden cookie')
-    except:
+    except Exception as e:
         pass
 
 
@@ -25,7 +26,7 @@ driver.get("https://orteil.dashnet.org/cookieclicker/")
 print(driver.title)
 
 try:
-    main = WebDriverWait(driver,20).until(
+    main = WebDriverWait(driver, 20).until(
         # EC.presence_of_all_elements_located((By.ID, 'prefsButton'))
         EC.presence_of_all_elements_located((By.ID, 'bigCookie'))
     )
@@ -38,48 +39,40 @@ except:
     print('[X] err')
     sleep(5)
     driver.quit()
+    print('[!] Bye')
+    sys.exit()
 
 driver.find_element_by_id('prefsButton').click()
 saving_path = 'C:/Users/tim10n/Downloads/CookieBakery.txt'
 driver.find_element_by_id('FileLoadInput').send_keys(saving_path)
 
-#get all the items ava to buy
+# get all the items ava to buy
 while True:
-    items=driver.find_elements_by_class_name('product.unlocked.enabled')
+    try:
+        items = driver.find_elements_by_class_name('product.unlocked.enabled')
 
-    clickGoldenCookie()
-    for i in reversed(items[8:]):
-        i.click()
-        print('clicked : ' + i.find_element_by_class_name('title').text +
-              '['+ i.find_element_by_class_name('title.owned').text +']')
-    # sleep(10)
-    for i in range(70):
-        driver.find_element(By.ID, 'bigCookie').click()
-    sleep(3)
+        click_golden_cookie()
+        for i in reversed(items[10:]):
+            i.click()
+            print('clicked : ' + i.find_element_by_class_name('title').text +
+                  '[' + i.find_element_by_class_name('title.owned').text + ']')
+        # sleep(10)
+        for i in range(70):
+            driver.find_element(By.ID, 'bigCookie').click()
+        sleep(3)
+    except ElementClickInterceptedException as e:
+        print(e)
+    except Exception as e:
+        print(e)
+        # export to save bt
+        # if (driver.)
+        driver.find_element_by_id('prefsButton').click()
+        print('clicked Options bt')
 
+        driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[18]/div[2]/div[4]/div[3]/div[3]/a[1]').click()
+        # saving record
+        text_record = driver.find_element_by_xpath('/html/body/div/div[2]/div[11]/div/div[1]/div[2]/textarea').text
 
-
-# search = driver.find_element_by_name("q")   #this is to find the search tag
-# search.send_keys("test", Keys.ARROW_DOWN)  #this is the text wnat to search
-# search.send_keys(Keys.RETURN)   #this means enter
-#
-# try:
-#     main = WebDriverWait(driver,10).until(
-#         EC.presence_of_all_elements_located((By.ID, "search"))
-#     )
-#
-#     print('[o] hey u found it')
-#     # print(driver.page_source)
-# except:
-#     print('[X] err')
-#
-# sleep(1)
-#
-# # results = driver.find_element_by_class_name('g')
-# results = driver.find_elements_by_class_name('g')
-# # print(results.text)
-# print(len(results))
-# sleep(5)
-# driver.quit()
-
-# driver.find_element()
+        with open('C:/Users/tim10n/Downloads/CookieBakery2.txt', 'w') as f:
+            f.write(text_record)
+        print('[!] saved record')
